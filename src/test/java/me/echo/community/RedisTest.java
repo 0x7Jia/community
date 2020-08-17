178ac16fd804db2586e94339ae913ad1ba40fbd5
MyHusky <MyHasky@hotmail.com>
2020-08-17 10:20:54 +0800

    配置好服务层自动记录日志功能、整合redis完成

---
+++ src/test/java/me/echo/community/RedisTest.java
@@ -0,0 +1,74 @@
+package me.echo.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testString(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHashes(){
+        String redisKey = "test:user";
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "jiaqi");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    @Test
+    public void testBoundOperations(){
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    // 编程式事务
+    @Test
+    public void testTransaction(){
+    Object object =
+        redisTemplate.execute(
+            new SessionCallback() {
+              @Override
+              public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "test:tx";
+                redisOperations.multi();
+
+                redisOperations.opsForSet().add(redisKey, "张三");
+                redisOperations.opsForSet().add(redisKey, "李四");
+                redisOperations.opsForSet().add(redisKey, "王五");
+                redisOperations.opsForSet().add(redisKey, "赵六");
+
+                System.out.println(redisOperations.opsForSet().members(redisKey));
+                return redisOperations.exec();
+              }
+            });
+        System.out.println(object);
+    }
+}
