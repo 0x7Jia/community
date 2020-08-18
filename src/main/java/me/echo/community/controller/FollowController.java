520d9d27509a1481b9f1f96e362daa869c983bcd
MyHusky <MyHasky@hotmail.com>
2020-08-18 11:39:09 +0800

    完成粉丝列表、关注列表页面

--- src/main/java/me/echo/community/controller/FollowController.java
+++ src/main/java/me/echo/community/controller/FollowController.java
@@ -1,22 +1,34 @@
 package me.echo.community.controller;
 
+import me.echo.community.entity.Page;
 import me.echo.community.entity.User;
 import me.echo.community.service.FollowService;
+import me.echo.community.service.UserService;
+import me.echo.community.util.CommunityConstant;
 import me.echo.community.util.CommunityUtil;
 import me.echo.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
 
     @Autowired
     private FollowService followService;
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
 
     /**
@@ -46,5 +58,81 @@ public class FollowController {
         User user = hostHolder.getUser();
         followService.unfollow(user.getId(), entityType, entityId);
         return CommunityUtil.getJSONString(0, "已取消关注");
+    }
+
+
+    /**
+     * 获取关注列表
+     * @param userId
+     * @param page
+     * @param model
+     * @return
+     */
+    @GetMapping("/followees/{userId}")
+    public String getFollowees(@PathVariable("userId") Integer userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if (user==null){
+            throw new RuntimeException("该用户不存在:"+userId);
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(20);
+        page.setPath("/followees/"+userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if (userList!=null){
+            for (Map<String, Object> map:userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+        return "/site/followee";
+    }
+
+
+    /**
+     * 获取粉丝列表
+     * @param userId
+     * @param page
+     * @param model
+     * @return
+     */
+    @GetMapping("/followers/{userId}")
+    public String getFollowers(@PathVariable("userId") Integer userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if (user==null){
+            throw new RuntimeException("该用户不存在:"+userId);
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(20);
+        page.setPath("/followers/"+userId);
+        page.setRows((int) followService.findFollowerCount(ENTITY_TYPE_USER, userId));
+
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if (userList!=null){
+            for (Map<String, Object> map:userList){
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+        return "/site/follower";
+    }
+
+    /**
+     * 获取当前登录的用户是否关注了目标用户
+     * @param id 目标用户id
+     * @return 是否关注，未登录则返回未关注
+     */
+    private boolean hasFollowed(Integer id) {
+        User user = hostHolder.getUser();
+        if (user==null){
+            return false;
+        }
+
+        return followService.hasFollowed(user.getId(), ENTITY_TYPE_USER, id);
     }
 }
--- src/main/java/me/echo/community/service/FollowService.java
+++ src/main/java/me/echo/community/service/FollowService.java
@@ -1,5 +1,6 @@
 package me.echo.community.service;
 
+import me.echo.community.util.CommunityConstant;
 import me.echo.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -8,10 +9,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
-public class FollowService {
+public class FollowService implements CommunityConstant {
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserService userService;
 
     /**
      * 关注
@@ -95,4 +101,55 @@ public class FollowService {
         return redisTemplate.opsForZSet().score(followeeKey, entityId)!=null;
     }
 
+
+    /**
+     * 查询某用户关注的人
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowees(Integer userId, int offset, int limit){
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+        Set<Integer> targetIds = redisTemplate.opsForZSet().reverseRange(followeeKey, offset, offset+limit-1);
+        if (targetIds==null){
+            return null;
+        }
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer id:targetIds){
+            Map<String, Object> map = new HashMap<>();
+            map.put("user", userService.findUserById(id));
+            Double score = redisTemplate.opsForZSet().score(followeeKey, id);
+            map.put("followTime", new Date(score.longValue()));
+
+            list.add(map);
+        }
+        return list;
+    }
+
+    /**
+     * 查询某用户的粉丝
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Map<String, Object>> findFollowers(Integer userId, int offset, int limit){
+        String followerKey = RedisKeyUtil.getFollowerKey(ENTITY_TYPE_USER, userId);
+        Set<Integer> followerId = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset + limit - 1);
+        if (followerId==null){
+            return null;
+        }
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Integer id:followerId){
+            Map<String, Object> map = new HashMap<>();
+            map.put("user", userService.findUserById(id));
+            Double score = redisTemplate.opsForZSet().score(followerKey, id);
+            map.put("followTime", new Date(score.longValue()));
+
+            list.add(map);
+        }
+
+        return list;
+    }
 }
